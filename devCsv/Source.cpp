c2bcd97243c44751b4e1ba80edd2a407b1ba6c1c
Ben <dreamvb@outlook.com>
2021-05-25 20:26:58 +0100

    Version 2.0

--- devCsv/Source.cpp
+++ devCsv/Source.cpp
@@ -1,5 +1,6 @@
 /*
-	Csv parser for C++ for reading only
+	Csv parser for C++ for reading and writeing
+	Version 2.0
 	By Ben a.k.a DreamVB
 */
 
@@ -11,30 +12,61 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 	CDevCsv csv;
+	vector<string>Cols;
+	vector<string>headers;
 
-	if (argc != 2){
-		std::cout << "Use " << argv[0] << " filename.csv" << std::endl;
+	csv.set_seperator(',');
+
+	//Setup header names
+	headers.push_back("name");
+	headers.push_back("gender");
+	headers.push_back("age");
+	//Add the new headers
+	csv.new_headers(headers);
+
+	//Add new record
+	Cols.push_back("Jenny");
+	Cols.push_back("Female");
+	Cols.push_back("32");
+	//Add new record
+	csv.add_record(Cols);
+
+	//Add next record
+	Cols.push_back("Mat");
+	Cols.push_back("male");
+	Cols.push_back("44");
+	//Add the new record
+	csv.add_record(Cols);
+
+	//Save file file name.
+	if (!csv.save("persons.csv")){
+		std::cout << "Error creating csv file." << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	
-	//Open csv file.
-	csv.open(argv[1]);
+
+	csv.clear();
+	//Load csv file
+
+	csv.open("persons.csv");
 
 	if (!csv.is_good){
-		std::cerr << "Error loading source csv filename" << std::endl;
+		std::cout << "Faild the open csv file." << std::endl;
 		exit(EXIT_FAILURE);
 	}
+	//Change the second records age.
+	csv.set_field_value<int>(1, "age", 65);
 
-	//Print the name of the second header
-	std::cout << "Make" << std::endl << "..........." << std::endl;
-	//Print the car make types.
+	//Display the records we just opened
 	for (int x = 0; x < csv.record_count(); x++){
-		std::cout << csv.get_field_value<string>(x, "Make") << std::endl;
+		std::cout << csv.get_field_value<string>(x, "name") << "\t"
+			<< csv.get_field_value<string>(x, "gender") << "\t"
+			<< csv.get_field_value<string>(x,"age") << std::endl;
 	}
-	std::cout << endl << "Year" << std::endl << "..........." << std::endl;
-	//Print the car make year.
-	for (int x = 0; x < csv.record_count(); x++){
-		std::cout << csv.get_field_value<int>(x, 0) << std::endl;
-	}
+
+
+	//Clear up
+	csv.clear();
+
+	system("pause");
 	return EXIT_SUCCESS;
 }
--- devCsv/devcsv.h
+++ devCsv/devcsv.h
@@ -1,5 +1,6 @@
 /*
-	Csv parser for C++ for reading only
+	Csv parser for C++ for reading and writeing
+	Version 2.0
 	By Ben a.k.a DreamVB
 */
 
@@ -57,7 +58,7 @@ private:
 		rtrim(s);
 	}
 	//Convert all the strings in vector to uppercase
-	const void uppercase_headers(){
+	const void uppercase_headers(void){
 		for (auto &s : headers){
 			for (auto &c : s){
 				c = toupper(c);
@@ -128,20 +129,25 @@ public:
 	//Used to test if the csv file opened.
 	bool is_good = false;
 
-	const void open(std::string Filename, 
-		unsigned seperator = ','){
+	const void open(std::string Filename){
 
 		fstream csv_file;
 		std::string row;
 		bool Firstline = false;
-		this->seperator = seperator;
 		//Filename to open
 		csv_file.open(Filename, std::ios::in);
 		//Test if the filename was opened
 		is_good = csv_file.is_open();
 		//Check if the file was opened.
 		if (is_good){
+			headers.clear();
+			rows.clear();
 			while (std::getline(csv_file, row)){
+				//check if the file is ok
+				if (csv_file.bad() || csv_file.fail()){
+					is_good = false;
+					break;
+				}
 				//Trim the line of spaces
 				trim(row);
 				//See if the line is not empty
@@ -165,7 +171,40 @@ public:
 		//Close file.
 		csv_file.close();
 	}
-	//
+
+	bool save(const std::string file) const{
+		fstream csv_file;
+		std::string fisrtline;
+
+		//Open the file for writeing.
+		csv_file.open(file, std::ios::out);
+		
+		if (!csv_file.good()){
+			return false;
+		}
+		else{
+			//Get the headers as the first line
+			for (size_t t = 0; t < headers.size(); t++){
+				if (t < headers.size() - 1){
+					fisrtline += headers[t];
+					fisrtline.push_back(seperator);
+				}
+				else{
+					fisrtline += headers[t];
+				}
+			}
+			//Add the headers to the data vector.
+			csv_file << fisrtline << std::endl;
+			//Add the records
+			for (size_t t = 0; t < rows.size(); t++){
+				csv_file << rows[t] << std::endl;
+			}
+		}
+		//Close the file.
+		csv_file.close();
+		return true;
+	}
+
 	template <typename T>
 	const T get_field_value(unsigned int row, unsigned int field){
 		T ret;
@@ -230,16 +269,111 @@ public:
 		return ret;
 	}
 
-	const size_t record_count(){
+	template <typename T>
+	void set_field_value(size_t row, std::string key, T data){
+		std::string fname = key;
+		std::string s_row;
+		std::vector<string>vals;
+		std::stringstream ss;
+		T Ret;
+		size_t f_idx;
+
+		//Uppercase fname
+		std::transform(fname.begin(), fname.end(), fname.begin(), ::toupper);
+		//Find string in vector
+		auto it = find(headers.begin(), headers.end(), fname);
+		//Check if string was found on vector
+		if (it != headers.end()){
+			//Work out the position of the string in the vector
+			f_idx = it - headers.begin();
+		}
+		else{
+			throw Error("Key Not Found.");
+		}
+
+		if (row < rows.size()){
+			std::string s_rec;
+			s_rec = rows[row];
+			//
+			vals = get_row_fields(s_rec);
+			//Set the vals data
+			ss << data;
+			ss >> vals[f_idx];
+
+			//Clear row data
+			s_row.clear();
+			//Build the new record from the vals vector.
+			for (int t = 0; t < vals.size(); t++){
+				if (t < vals.size() - 1) {
+					s_row += vals[t];
+					s_row.push_back(seperator);
+				}
+				else{
+					s_row += vals[t];
+				}
+			}
+			//Check for empry row
+			if (!s_row.empty()){
+				rows[row] = s_row;
+			}
+			else{
+				throw Error("Record Is Empty");
+			}
+		}
+		vals.clear();
+		ss.clear();
+	}
+
+	const void add_record(std::vector<string>&colnums){
+		std::string s_rec;
+
+		if (colnums.empty()){
+			throw Error("No Colunms To Add.");
+		}
+		if (colnums.size() > headers.size()){
+			throw Error("Colunms Out Of Range.");
+		}
+		//
+		for (int x = 0; x < colnums.size();x++){
+			if (x < colnums.size() - 1){
+				s_rec += colnums[x];
+				s_rec.push_back(seperator);
+			}
+			else{
+				s_rec += colnums[x];
+			}
+		}
+		//Add the new record.0.
+
+		rows.push_back(s_rec);
+		//Clear the colunms ater each reocrd add.
+		colnums.clear();
+	}
+
+	const void delete_record(size_t rec){
+		if (rec < rows.size()){
+			rows.erase(rows.begin() + rec);
+		}
+		else{
+			throw Error("No Record To Delete");
+		}
+	}
+
+	size_t record_count(void) const{
 		//Return the number of rows in the csv file.
 		return this->rows.size();
 	}
-	const size_t field_count(){
-		//Return the number of fields in the csv file.
+	size_t field_count(void) const{
+		//Return the number of fields in a single record same as headers count
 		return this->headers.size();
 	}
 
-	const std::string get_header(size_t index) const{
+	size_t header_count(void) const{
+		//Return the number of headers in the csv file.
+		return this->headers.size();
+	}
+
+	const std::string get_header(const size_t index) const{
 		if (index >= headers.size()){
 			throw Error("Header Index Out Of Range.");
 		}
@@ -247,6 +381,84 @@ public:
 			//Return header name
 			return headers.at(index);
 		}
+	}
+
+	//Add headers
+	const void new_headers(const std::vector<string>colnums){
+		std::string s_rec;
+
+		if (colnums.empty()){
+			throw Error("No Headers To Add.");
+		}
+		//Add to headers vector.
+		headers = colnums;
+		uppercase_headers();
+	}
+
+	const void set_header(size_t index, const std::string name){
+		std::string fname = name;
+		//Uppercase the header name.
+		std::transform(fname.begin(), fname.end(), fname.begin(), ::toupper);
+
+		if (index >= headers.size()){
+			throw Error("Header Index Out Of Range.");
+		}
+		else{
+			//Set header name
+			headers[index] = fname;
+		}
+	}
+
+	const void delete_header(size_t index){
+		std::vector<string>temp;
+		std::vector<string>temp_rows;
+		std::string s_rec;
+		//Delete header
+
+		if (index >= headers.size()){
+			throw Error("Header Index Out Of Range.");
+		}
+		else{
+			//Next we need to delete the record field.
+			for (size_t t = 0; t < rows.size(); t++){
+				temp = get_row_fields(rows[t]);
+				//Delete the item from temp at position index.
+				temp.erase(temp.begin() + index);
+				
+				//Re-build the records
+				for (size_t n = 0; n < temp.size();n++){
+					if (n < temp.size() - 1){
+						s_rec += temp[n];
+						s_rec.push_back(seperator);
+					}
+					else{
+						s_rec += temp[n];
+					}
+				}
+				//Add to temp record vector
+				temp_rows.push_back(s_rec);
+				//Clear buffer
+				s_rec.clear();
+			}
+			//
+		}
+		//Delete the header
+		headers.erase(headers.begin() + index);
+		//Set the new rows
+		rows = temp_rows;
+
+		//Clear up
+		temp.clear();
+		temp_rows.clear();
+	}
+
+	const void set_seperator(const unsigned char sep){
+		this->seperator = sep;
+	}
+
+	const void clear(void){
+		headers.clear();
+		rows.clear();
 	}
 
 	~CDevCsv(){
